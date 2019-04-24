566a656f257fe6c6041d7c008fe93dac18be52c3
eslam elaraby <islam.ismail038@gmail.com>
2019-04-24 17:49:16 +0200

    simple example for Async-await

---
+++ script.js
@@ -0,0 +1,24 @@
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", function() {
+  getData()
+    .then(data => showData(data))
+    .catch(error => console.log(error));
+});
+
+function showData(data) {
+  const {
+    name: { first },
+    dob: { age }
+  } = data.results[0];
+
+  document.getElementById("name").textContent = first;
+  document.getElementById("age").textContent = age;
+}
+
+const getData = async function() {
+  const url = "https://randomuser.me/api/";
+  const result = await fetch(url);
+  const response = await result.json();
+  return response;
+};
